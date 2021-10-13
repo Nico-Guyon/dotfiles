b2c844bbd03de67d815833f274bfc48f2329d0f5
Nicolas Guyon <shinkschn@gmail.com>
2021-10-13 10:11:09 +0200

    workaround for react/index.d.ts jumpToDef problem
    - added a small workaround in order to filter jumpToDefinition results
      send by tsserver and remove react/index.d.ts which is send nearly
      everytime

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -25,6 +25,37 @@ _G.lsp_organize_imports = function()
     vim.lsp.buf.execute_command(params)
 end
 
+-- small workaround to prevent nvim to display react/index.d.ts 
+-- every time on each jumpToDefinition ...
+-- https://github.com/typescript-language-server/typescript-language-server/issues/216
+vim.lsp.handlers["textDocument/definition"] = function(_, method, result)
+  if result == nil or vim.tbl_isempty(result) then
+     local _ = vim.lsp.log.info() and vim.lsp.log.info(method, 'No location found')
+     return nil
+  end
+
+  if vim.tbl_islist(result) then
+     vim.lsp.util.jump_to_location(result[1])
+     if #result > 1 then
+        local isReactDTs = false
+        for key, value in pairs(result) do
+           if string.match(value.uri, "react/index.d.ts") then
+              isReactDTs = true
+              break
+           end
+        end
+        if not isReactDTs then
+           vim.lsp.util.set_qflist(util.locations_to_items(result))
+           vim.api.nvim_command("copen")
+           vim.api.api.nvim_command("wincmd p")
+        end
+     end
+  else
+     vim.lsp.util.jump_to_location(result)
+  end
+end
+
+
 -- https://rishabhrd.github.io/jekyll/update/2020/09/19/nvim_lsp_config.html
 local on_attach = function(client, bufnr)
     local buf_map = vim.api.nvim_buf_set_keymap
